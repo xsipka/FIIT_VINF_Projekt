e0803d89e580d2ca4e6d4a5622598183d606a44a
Adam <sipka.adam@gmail.com>
2021-10-17 22:32:28 +0200

    lolololololololol

--- my_indexer.py
+++ my_indexer.py
@@ -1,11 +1,9 @@
 import csv
 import re
 import unidecode
-from nltk.corpus import stopwords
+#from nltk.corpus import stopwords
 from math import log
-from nltk.stem import PorterStemmer
-from nltk.tokenize import TweetTokenizer
-import string
+
 
 FILE_00 =  'output_wiki_13.csv'
 FILE_01 = 'output_wiki_11.csv'
@@ -20,7 +18,10 @@ def removeStopWord(document, stopWords):
     document = unidecode.unidecode(document)
     document = document.lower()
     document = re.sub(r'[^a-z0-9|()*]+', ' ', document)
+    document = re.sub(r'(list|group)[1-9]', '', document)
+    document = re.sub(r'style font style (normal|italic)', '', document)
     document = ' '.join([word for word in document.split() if word not in stopWords])
+    #print(document)
     return document
 
 
@@ -62,7 +63,14 @@ def calculateTfIdf(term, termDict, numOfDocs):
         if isinstance(termDict[key], int) == False:
             tf = termDict[key]['termFreq']
             tfIdf = tf * idf
-            tfIdfDict[term, key] = tfIdf
+            if tfIdf <= 0:
+                tfIdf = 0
+
+            if term not in tfIdfDict:
+                tfIdfDict[term] = {}
+                tfIdfDict[term][int(key)] = tfIdf
+            elif term in tfIdfDict:
+                tfIdfDict[term][int(key)] = tfIdf
 
     return tfIdfDict
 
@@ -70,22 +78,64 @@ def calculateTfIdf(term, termDict, numOfDocs):
 # main
 if __name__ == "__main__":
 
-    myList = ['Navbox', 'navbox', 'Infobox', 'infobox', 'getArgs','args']
-    stopWords = stopwords.words("english")
-    stopWords.extend(myList)
+    myStopWords = ['Navbox', 'navbox', 'Infobox', 'infobox', 'getArgs', 'args', 'listclass', '|listclass',
+                   'br', 'hlist', 'autocollapse', 'imagesize', 'caption', '|hlist', 'hlist|', 'listclass|']
+    #stopWords = stopwords.words("english")
+    #stopWords.extend(myList)
 
     termDict = {}
     tfIdfDict = {}
     numOfDocs = 0
 
-    with open(PATH + FILE_02, 'r', encoding='utf-8') as file:
+    with open(PATH + FILE_01, 'r', encoding='utf-8') as file:
         csv_reader = csv.DictReader(file)
 
         for row in csv_reader:
             document = row['title'] + row['text']
-            document = removeStopWord(document, stopWords)
+            document = removeStopWord(document, myStopWords)
             termDict = createTermDict(row['id'], document, termDict)
             numOfDocs += 1
 
     for key in termDict:
         tfIdfDict.update(calculateTfIdf(key, termDict[key], numOfDocs))
+
+    # for key in tfIdfDict:
+    #    print(key, tfIdfDict[key])
+
+    # vyhladavanie
+    while True:
+        query = input("Search for: ")
+        query = query.lower()
+
+        avgTfIdf = []
+        found = []
+        shared = []
+
+        if query == 'q':
+            break
+        else:
+            try:
+                for term in query.split():
+                    if term.isalnum() and term in tfIdfDict:
+                        found.append(tfIdfDict[term])
+
+                for item in found:
+
+                    if len(shared) == 0:
+                        shared = list(item.keys())
+
+                    temp = list(item.keys())
+                    shared = (list(set(shared).intersection(temp)))
+
+                for doc in shared:
+                    temp = 0
+                    for item in found:
+                        temp += item[doc]
+                    avgTfIdf.append((doc, temp/len(shared)))
+
+                avgTfIdf.sort(key=lambda x:x[1])
+                for doc in avgTfIdf:
+                    print("Check out document", doc[0])
+
+            except:
+                print("Nothing relevant found ...")
