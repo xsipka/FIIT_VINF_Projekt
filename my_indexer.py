e68a7bf28639a149ec9313793b1d50bb2d842653
Adam Šípka <43640571+xsipka@users.noreply.github.com>
2021-10-19 20:51:10 +0200

    ye

--- my_indexer.py
+++ my_indexer.py
@@ -1,26 +1,24 @@
 import csv
 import re
 import unidecode
-from nltk.corpus import stopwords
+#from nltk.corpus import stopwords
 from math import log
-from nltk.stem import PorterStemmer
-from nltk.tokenize import TweetTokenizer
-import string
+
 
 FILE_00 =  'output_wiki_13.csv'
 FILE_01 = 'output_wiki_11.csv'
 FILE_02 = 'output_wiki_conrad.csv'
 FILE_03 = 'output_wiki_war_and_peace.csv'
 PATH = 'datasets/'
-BASE = 10
+LOG_BASE = 10
 
 
 # removes stop words and most of nonalphanumeric characters
 def removeStopWord(document, stopWords):
     document = unidecode.unidecode(document)
     document = document.lower()
-    document = re.sub(r'[^a-z0-9|()*]+', ' ', document)
     document = ' '.join([word for word in document.split() if word not in stopWords])
+    #print(document)
     return document
 
 
@@ -55,37 +53,134 @@ def createTermDict(id, document, termDict):
 def calculateTfIdf(term, termDict, numOfDocs):
 
     docFreq = termDict['docFreq']
-    idf = log(numOfDocs/docFreq, BASE)
+    idf = log(numOfDocs/docFreq, LOG_BASE)
     tfIdfDict = {}
 
     for key in termDict:
         if isinstance(termDict[key], int) == False:
             tf = termDict[key]['termFreq']
             tfIdf = tf * idf
-            tfIdfDict[term, key] = tfIdf
+            if tfIdf <= 0:
+                tfIdf = 0
+
+            if term not in tfIdfDict:
+                tfIdfDict[term] = {}
+                tfIdfDict[term][int(key)] = tfIdf
+            elif term in tfIdfDict:
+                tfIdfDict[term][int(key)] = tfIdf
 
     return tfIdfDict
+
+
+# read found documents from file
+def readFoundDocuments(offsets, docsToCheck):
+
+    with open(PATH + FILE_01, 'r', encoding='utf-8') as file:
+
+        for doc in docsToCheck:
+            file.seek(offsets[doc[0]], 0)
+            line = file.readline()
+            formatOutput(line)
+
+
+# format output in a naice way
+def formatOutput(document):
+    document = document.replace(',', '\n')
+    document = document.replace(' br ', ' ')
+    docList = document.split('\n')
+    docList[2] = docList[2].replace('=', ':')
+    type = re.findall(r'^[^\|]+ *\||$', docList[2])[0]
+    docList[2] = re.sub('^[^\|]+ *\|', '', docList[2])
+    docList[2] = docList[2].lstrip()
+    finalDoc = docList[2].split('|')
+
+    print('Document ID:', docList[0])
+    print('Document name:', docList[1])
+    print('Document type:', type.replace('|', ''))
+
+    prev = ''
+    newLine = ''
+    for index, currItem in enumerate(finalDoc):
+        if ':' in prev and ':' in currItem:
+            newLine = '\n'
+
+        if ':' in currItem:
+            print(newLine,currItem, end='')
+            prev = currItem
+            newLine = ''
+        else:
+            print(newLine,currItem, end='')
+
+    print('\n')
 
 
 # main
 if __name__ == "__main__":
 
-    myList = ['Navbox', 'navbox', 'Infobox', 'infobox', 'getArgs','args']
-    stopWords = stopwords.words("english")
-    stopWords.extend(myList)
+    myStopWords = ['Infobox', 'infobox', 'getArgs', 'args', 'listclass', '|listclass', 'br',
+                   'hlist', 'autocollapse', 'imagesize', 'caption', '|hlist', 'hlist|', 'listclass|']
+    #stopWords = stopwords.words("english")
+    #stopWords.extend(myList)
 
     termDict = {}
     tfIdfDict = {}
+    fileOffsets = []
     numOfDocs = 0
 
-    with open(PATH + FILE_02, 'r', encoding='utf-8') as file:
+    # vyratanie a ulozenie offsetov, nech vieme kde sa dokument zacina
+    with open(PATH + FILE_01, 'rb') as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            fileOffsets.append(file.tell())
+
+    # indexovanie
+    with open(PATH + FILE_01, 'r', encoding='utf-8') as file:
         csv_reader = csv.DictReader(file)
 
         for row in csv_reader:
             document = row['title'] + row['text']
-            document = removeStopWord(document, stopWords)
+            document = removeStopWord(document, myStopWords)
             termDict = createTermDict(row['id'], document, termDict)
             numOfDocs += 1
 
     for key in termDict:
         tfIdfDict.update(calculateTfIdf(key, termDict[key], numOfDocs))
+
+    # vyhladavanie
+    while True:
+        query = input("Search for: ")
+        query = query.lower()
+
+        avgTfIdf = []
+        found = []
+        shared = []
+
+        if query == 'q':
+            break
+        else:
+            try:
+                for term in query.split():
+                    if term.isalnum() and term in tfIdfDict:
+                        found.append(tfIdfDict[term])
+
+                for item in found:
+
+                    if len(shared) == 0:
+                        shared = list(item.keys())
+
+                    temp = list(item.keys())
+                    shared = (list(set(shared).intersection(temp)))
+
+                for doc in shared:
+                    temp = 0
+                    for item in found:
+                        temp += item[doc]
+                    avgTfIdf.append((doc, temp/len(shared)))
+
+                avgTfIdf.sort(key=lambda x:x[1])
+                readFoundDocuments(fileOffsets, avgTfIdf)
+
+            except:
+                print("Nothing relevant found ...")
--- my_parser.py
+++ my_parser.py
@@ -3,12 +3,14 @@ import csv
 import os
 import time
 import re
+import unidecode
 
 FILE_00 =  'en_wiki_13.xml'
 FILE_01 = 'en_wiki_11.xml-p6899367p7054859'
 FILE_02 = 'conrad_wiki.xml'
 FILE_03 = 'war_and_peace_wiki.xml'
-PATH = 'datasets/'
+IN_PATH = 'datasets/raw data/'
+OUT_PATH = 'datasets/'
 MAX_LIST_LENGTH = 100000
 
 
@@ -18,6 +20,17 @@ def timeFormater(elapsedTime):
     mins = int((elapsedTime % (60 * 60)) / 60)
     secs = elapsedTime % 60
     return "{}:{:>02}:{:>05.2f}".format(hour, mins, secs)
+
+
+# removes excess whitespace from string + other stuff
+def stringFormater(string):
+    string = unidecode.unidecode(string)
+    string = string.lower()
+    string = re.sub(r'[\"\[\]]', ' ', string)
+    string = re.sub(r' +', ' ', string)
+    string = re.sub(r'[^a-z0-9|()*=]+', ' ', string)
+    return string
+
 
 # class used for parsing XML file page by page
 class PageHandler(xml.sax.handler.ContentHandler):
@@ -32,6 +45,7 @@ class PageHandler(xml.sax.handler.ContentHandler):
         self.contentHelper = False
         self.pageSaver = False
 
+
     # add retrieved content into buffer
     def characters(self, content):
         if self.element == 'title':
@@ -40,11 +54,13 @@ class PageHandler(xml.sax.handler.ContentHandler):
         if self.element == 'text' and self.findUsefulData(content):
             self.buffer.append(content)
 
+
     # check if starting tag of element whose content we want to retrieve (title or text)
     def startElement(self, element, attrs):
         if element in ('title', 'text'):
             self.element = element
             self.buffer = []
+
 
     # check if ending tag of element, save retrieved content
     def endElement(self, element):
@@ -62,9 +78,10 @@ class PageHandler(xml.sax.handler.ContentHandler):
             self.pageList = []
             print("100k hotovo :)")
 
+
     # parse Infoboxes and Navboxes using regular expressions
     def findUsefulData(self, content):
-        if re.search('(?i){{Infobox (book|manuscript|short story|writer|film)', content) or re.search('(?i){{Navbox', content):
+        if re.search('(?i){{Infobox *(book|short story|writer|film)', content) or re.search('(?i){{Navbox', content):
             self.contentParser = True
             self.pageSaver = True
             return True
@@ -86,18 +103,92 @@ class PageHandler(xml.sax.handler.ContentHandler):
 
         return False
 
+
+    # extrac useful data out of infoboxes & navboxes
+    def processPage(self, page):
+
+        if re.search('(?i){{Infobox *(book|short story|writer|film)', page):
+            data = self.extractInfoboxData(page)
+            return data
+
+        elif re.search('(?i){{Navbox', page):
+            data = self.extractNavboxData(page)
+            return data
+
+        return page
+
+
+    # extract data out of infoboxes by using regular expressions
+    def extractInfoboxData(self, page):
+        data = []
+
+        # select from infobox book or short story
+        if re.search('(?i){{Infobox *(book|short story)', page):
+            toSave = 'book | '
+            data.append(re.findall('(?i)name *=.*?(?=\|)\|', page))
+            data.append(re.findall('(?i)author *=.*?(?=\])\] *\|', page))
+            data.append(re.findall('(?i)genre *=.*?(?=\])\] *\|', page))
+            data.append(re.findall('(?i)pages *=.*?(?=\|)\|', page))
+
+            for item in data:
+                toSave += ''.join(item)
+            return toSave
+
+        # select from infobox writer
+        elif re.search('(?i){{Infobox *writer', page):
+            toSave = 'writer | '
+            data.append(re.findall('(?i)[ *|\|]name *=.*?(?=\|)\|', page))
+            data.append(re.findall('(?i)pseudonym *=.*?(?=\|)\|', page))
+
+            for item in data:
+                toSave += ''.join(item)
+            return toSave
+
+        # select from infobox film
+        elif re.search('(?i){{Infobox *film', page):
+            toSave = 'film | '
+            data.append(re.findall('(?i)name *=.*?(?=\|)\|', page))
+            data.append(re.findall('(?i)director *=.*?(?=\])\] *\|', page))
+            #data.append(re.findall('(?i)based on *\|.*?(?=\}\})\}\} *\|', page))
+            data.append(re.findall('(?i)based_on *=.*?(?=\}\})\}\} *\|', page))
+
+            for item in data:
+                toSave += ''.join(item)
+            return toSave
+
+        return page
+
+
+    # extract data out of infoboxes by using regular expressions
+    def extractNavboxData(self, page):
+
+        data = []
+        toSave = 'navbox | '
+
+        data.append(re.findall('(?i)name *=.*?(?=\|)\|', page))
+        data.append(re.findall('(?i)group\d+ *=.*', page))
+
+        for item in data:
+            toSave += ''.join(item)
+        toSave = re.sub('(?i){{noitalic\|\(\d+\)\}\}', ' ', toSave)
+        return toSave
+
+
     # save found data in file in CSV format
     def saveListAsCSV(self):
         header = ['id', 'title', 'text']
 
-        with open(PATH + 'output.csv', 'a', encoding='utf-8') as file:
+        with open(OUT_PATH + 'output.csv', 'a', encoding='utf-8') as file:
             writer = csv.writer(file)
 
-            if os.path.getsize(PATH + 'output.csv') == 0:
+            if os.path.getsize(OUT_PATH + 'output.csv') == 0:
                 writer.writerow(header)
 
             for index, page in enumerate(self.pageList):
-                row = [index, page[0], page[1]]
+                title = str(page[0])
+                page = self.processPage(str(page[1]))
+                page = stringFormater(page)
+                row = [index, title, page]
                 writer.writerow(row)
 
 
@@ -108,7 +199,7 @@ if __name__ == "__main__":
     parser = xml.sax.make_parser()
     parser.setContentHandler(handler)
 
-    with open(PATH + FILE_03, encoding='utf-8') as file:
+    with open(IN_PATH + FILE_00, encoding='utf-8') as file:
         for line in file:
             parser.feed(line)
 
